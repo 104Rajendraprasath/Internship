422436cdc5d6939f3d265a292e33466c4c51f404
104Rajendraprasath <prasathrajendra2000@gmail.com>
2026-02-11 09:32:39 +0530

    lwc,lds,flows,triggers

--- workingWithDataInLWC/force-app/main/default/lwc/contactList/contactList.js
+++ workingWithDataInLWC/force-app/main/default/lwc/contactList/contactList.js
@@ -1,28 +1,13 @@
 import { LightningElement, wire } from 'lwc';
-
-// 1. Import the Apex method: @salesforce/apex/ClassName.methodName
-import getContacts from '@salesforce/apex/ContactController.getContacts';
-import FIRST_NAME_FIELD from '@salesforce/schema/Contact.FirstName';
-import LAST_NAME_FIELD from '@salesforce/schema/Contact.LastName';
-import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
-
-// 2. Define the columns for the lightning-datatable
-const COLUMNS = [
-    // fieldName must match the API name in the Apex query (FirstName)
-    { label: 'First Name', fieldName: 'FirstName', type: 'text' },
-    { label: 'Last Name', fieldName: 'LastName', type: 'text' },
-    { label: 'Email', fieldName: 'Email', type: 'email' }
-];
+import { reduceErrors } from 'c/ldsUtils';
+import GET_CONTACTS from '@salesforce/apex/ContactController.getContacts';
 
 export default class ContactList extends LightningElement {
-    // 3. Assign the column array to a property
-    columns = COLUMNS;
-    
-    // 4. Wire the Apex method to the 'contacts' property
-    // Since getContacts has no parameters, we just pass the function name
-    @wire(getContacts)
-    contacts; // The data will be in contacts.data or contacts.error
-     get errors() {
+    @wire(GET_CONTACTS)
+    contacts;
+
+    // This getter is MANDATORY for the challenge
+    get errors() {
         return (this.contacts.error) ?
             reduceErrors(this.contacts.error) : [];
     }
